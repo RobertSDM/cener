77c412139e5fd8fb00478cbccebc10d7ae6cf3d7
Roberto Menezes <roberto.menesantos@gmail.com>
2023-12-20 10:20:49 -0300

    feat: adding the frontend

---
+++ frontend/backend.js
@@ -0,0 +1,114 @@
+const form = document.getElementById("__link_form");
+const linkInput = document.getElementById("__link_input");
+const showShortLinkEl = document.getElementById("__show_short-link");
+const copyShortLink = document.getElementById("__copy_short-link");
+const containerLink = document.getElementById("__container-link");
+
+/**
+ * @type {"NOT_STARTED" | "ON_GOING" | "COMPLETED"}
+ */
+let CREATING_STATUS = "NOT_STARTED";
+
+/**
+ * @param {string} link
+ */
+const createLink = async (link) => {
+    CREATING_STATUS = "ON_GOING";
+    try {
+        const res = await fetch("https://cener.onrender.com/create", {
+            method: "POST",
+            body: JSON.stringify({
+                originalLink: link,
+            }),
+            headers: {
+                authorization: import.meta.env.VITE_AUTH_APIS,
+            },
+        });
+
+        const data = await res.json();
+        console.log(data);
+        if (res.status === 201) {
+            return data;
+        } else {
+            showShortLinkEl.innerText = "An error ocurred, try again";
+            showShortLinkEl.style["color"] = "red";
+            return null;
+        }
+    } catch (err) {
+        showShortLinkEl.innerText = "An error ocurred, try again";
+        showShortLinkEl.style["color"] = "red";
+    }
+};
+
+/**
+ *
+ * @param {string} link
+ * @returns {boolean}
+ */
+const validadeLink = (link) => {
+    const regex = /^(http|https):\/\/[^ "]+$/;
+
+    return link.match(regex);
+};
+
+const addLoadAnimation = () => {
+    showShortLinkEl.style["color"] = "black";
+    showShortLinkEl.innerText = "creating";
+    let dots = "";
+
+    let timer;
+    timer = setInterval(() => {
+        if (dots.length === 3) {
+            showShortLinkEl.innerText = "creating";
+            dots = "";
+        }
+
+        dots += ".";
+        showShortLinkEl.innerText = `creating ${dots}`;
+    }, 500);
+    return timer;
+};
+
+/**
+ * @param {Event} event
+ * @returns {void}
+ */
+const handleSubmit = (event) => {
+    event.preventDefault();
+    containerLink.classList.remove("hidden");
+    containerLink.classList.add("show-flex");
+
+    if (CREATING_STATUS === "ON_GOING") {
+        return;
+    }
+
+    const link = linkInput.value;
+
+    const timer = addLoadAnimation();
+
+    if (validadeLink(link)) {
+        createLink(link).then((res) => {
+            CREATING_STATUS = "COMPLETED";
+            clearInterval(timer);
+            if (res) {
+                showShortLinkEl.innerText = res["Content"].shortened_link;
+                copyShortLink.classList.remove("hidden");
+                copyShortLink.classList.add("show-block");
+            } else {
+            }
+        });
+    }
+};
+
+/**
+ * @param {void}
+ * @returns {void}
+ */
+const main = () => {
+    form.addEventListener("submit", handleSubmit);
+    copyShortLink.addEventListener("click", () => {
+        navigator.clipboard.writeText(showShortLinkEl.innerText);
+    });
+};
+
+main();
---
+++ frontend/main.js
@@ -0,0 +1,36 @@
+import "./style.css";
+
+const app = document.querySelector("#app");
+
+const form = `
+    <form action="" id="__link_form">
+        <input
+            id="__link_input"
+            type="text"
+            placeholder="Digit or paste your link"
+            minlength="35"
+            required
+        />
+        <button type="submit" class="__default_button">Short</button>
+    </form>`;
+
+const containerShowLinks = `
+    <div id="__container-show-short-link">
+        <span id="__show_short-link"></span>
+        <button
+            id="__copy_short-link"
+            class="__transparent_bg_button hidden"
+        >
+            <i class="fa-regular fa-copy"></i>
+        </button>
+    </div>`;
+
+app.innerHTML = `
+  <div class="__bubble __b-red"></div>
+        <div class="__bubble __b-blue"></div>
+        <h1>Short your link</h1>
+        ${form}
+        <div id="__container-link" class="hidden">
+            ${containerShowLinks}
+        </div>
+`;
